1625459a6a946ff4b25c6de7d8b5827241ec92ef
unknown <software@tectronix.cl>
2018-11-19 17:34:19 -0300

    v0.01

---
+++ src/pages/home/home.ts
@@ -0,0 +1,114 @@
+import { Component,NgZone } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
+import { MainPage } from '../main/main';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+
+@Component({
+	selector: 'page-home',
+  	templateUrl: 'home.html'
+})
+export class HomePage {
+
+	devices: any[] = [];
+  	statusMessage: string;
+  	peripheral: any = {};
+
+	constructor(public navCtrl: NavController,
+				private androidPermissions: AndroidPermissions,
+				private toastCtrl: ToastController,
+				private ble: BLE,
+				private ngZone: NgZone) {
+
+ 	}
+
+  	ionViewDidEnter() {
+  		console.log('ionViewDidEnter: HomePage');
+  	}
+
+  	ionViewDidLeave(){
+  		console.log('ionViewDidLeave: HomePage');
+  	}
+
+	scan(){
+		this.checkPermissions();
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.BLUETOOTH)
+		this.setStatus('Escaneando Dispositivos Bluetooth');
+		this.devices = [];
+
+		this.ble.scan([],5).subscribe(
+			device => this.onDeviceDiscovered(device),
+			error => this.scanError(error)
+		);
+
+		setTimeout(this.setStatus.bind(this),10000,'Escaneo completo');
+	}
+
+	checkPermissions(){
+		var resp: boolean;
+
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.BLUETOOTH).then(
+			result => {
+				console.log('Permiso? ',result.hasPermissions);
+			},
+			err => {
+				this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.BLUETOOTH);
+				console.log('Permiso? ',err.hasPermissions);
+			}
+		);
+
+		return resp
+	}
+
+	disconnect(){
+		this.ble.isConnected('84:68:3E:03:8E:55').then(
+			success => {
+				console.log('Dispositivo ' + this.peripheral.name + ' está conectado');
+				this.ble.disconnect(this.peripheral.id)
+				this.setStatus(this.peripheral.id + 'Desconectado');
+			},
+			failure => {
+				console.log('No se encuentra conectado a ningún dispositivo');
+				this.setStatus('No se encuentra conectado a ningún dispositivo');
+			}
+		);
+	}
+
+	setStatus(message){
+		console.log(message);
+		// this.ngZone.run(() => {
+		// 	this.statusMessage = message;
+		// });
+		let toast = this.toastCtrl.create({
+			message: message,
+			position: 'middle',
+			duration: 3000
+		});
+		toast.present();
+	}
+
+	onDeviceDiscovered(device){
+		console.log('Descubierto ' + JSON.stringify(device,null,2));
+		this.ngZone.run(() => {
+			this.devices.push(device);
+		});
+	}
+
+	scanError(error){
+		this.setStatus('Error ' + error);
+		let toast = this.toastCtrl.create({
+			message: 'Error al escanear dispositivos bluetooth',
+			position: 'middle',
+			duration: 3000
+		});
+		toast.present();
+	}
+
+	deviceSelected(device){
+		console.log(JSON.stringify(device) + 'seleccionado');
+		this.navCtrl.push(MainPage,{
+			device: device
+		});
+	}
+
+}
---
+++ src/pages/main/main.ts
@@ -0,0 +1,106 @@
+import { Component,NgZone } from '@angular/core';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
+
+const REPETITIONS_SERVICE = '03b80e5a-ede8-4b33-a751-6ce34ec4c700';
+const REPETITIONS_CHARACTERISTIC = '7772e5db-3868-4112-a1a9-f2669d106bf3';
+
+@Component({
+  selector: 'page-main',
+  templateUrl: 'main.html',
+})
+export class MainPage {
+
+	peripheral: any = {};
+	statusMessage: string;
+	repetitions: number;
+
+	constructor(public navCtrl: NavController, 
+					public navParams: NavParams,
+					private ble: BLE, 
+					private alertCtrl: AlertController,
+					private ngZone: NgZone) {
+
+		let device = navParams.get('device');
+
+		this.setStatus('Conectando a ' + device.name || device.id);
+
+		this.ble.connect(device.id).subscribe(
+			peripheral => this.onConnected(peripheral),
+			peripheral => this.showAlert('Desconectado','El dispositivo de desconectó inesperadamente')
+		);
+
+	}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad MainPage');
+	}
+
+	ionViewDidLeave(){
+		console.log('ionViewDidLeave MainPage');
+		this.ble.disconnect(this.peripheral.id);
+	}
+
+	onConnected(peripheral){
+		this.peripheral = peripheral;
+		console.log(peripheral.id);
+		this.setStatus('Conectado a ' + (peripheral.name || peripheral.id));
+
+		this.ble.startNotification(this.peripheral.id, REPETITIONS_SERVICE, REPETITIONS_CHARACTERISTIC).subscribe(
+			data => this.onRepetitionsChange(data),
+			() => this.showAlert('Error inesperado', 'Falla al suscribirse al conteo de repeticones')
+		);
+
+		this.ble.write(this.peripheral.id, REPETITIONS_SERVICE, REPETITIONS_CHARACTERISTIC, this.stringToBytes("1"));
+
+	}
+
+	// ASCII only
+	stringToBytes(string) {
+		var array = new Uint8Array(2);
+		
+		array[0] = string.charCodeAt(0);
+		console.log(array);
+
+		return array.buffer;
+	}
+
+	onRepetitionsChange(buffer: ArrayBuffer){
+
+		var data = new Uint8Array(buffer);
+
+		String.fromCharCode.apply(null, new Uint8Array(data));
+
+		console.log(String.fromCharCode.apply(null, new Uint8Array(data)));
+
+		this.ngZone.run(() => {
+			this.repetitions = String.fromCharCode.apply(null, new Uint8Array(data));
+		});
+
+	}
+
+	ionViewWillLeave(){
+		console.log('Desconectando Bluetooth');
+		this.ble.disconnect(this.peripheral.id).then(
+			() => console.log('Desconectado ' + JSON.stringify(this.peripheral)),
+			() => console.log('ERROR desconectando ' + JSON.stringify(this.peripheral))
+		)
+	}
+
+	setStatus(message){
+		console.log(message);
+		this.ngZone.run( ()=>{
+			this.statusMessage = message;
+		});
+	}
+
+	showAlert(title,message){
+		let alert = this.alertCtrl.create({
+			title: title,
+			subTitle: message,
+			buttons: ['OK']
+		});
+		alert.present();
+	}
+
+}
